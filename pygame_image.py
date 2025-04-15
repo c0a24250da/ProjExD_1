3d26ab88f118c20077cc5906e386b4cce365bb38
c0a24250da <c0a24250da@edu.teu.ac.jp>
2025-04-15 16:02:30 +0900

    練習 背景反転

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,True,False)
     k_img = pg.image.load("fig/3.png")
     k_img=pg.transform.flip(k_img,True,False)
     tmr = 0
@@ -18,7 +19,7 @@ def main():
             if event.type == pg.QUIT: return
         x = tmr
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img,[-x+15000,0])
+        screen.blit(bg_img2,[-x+15000,0])
         screen.blit(k_img,[300,200])
         pg.display.update()
         tmr += 1        
